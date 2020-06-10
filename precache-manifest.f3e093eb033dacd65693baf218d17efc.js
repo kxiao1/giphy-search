a37d77923de308db7e690d13c8e5f650457119df
kxiao1 <kxiao1@users.noreply.github.com>
2020-06-10 07:35:34 +0000

    deploy: 82d076233514c753390e8a4dd3aa4fc17a56ee40

---
+++ precache-manifest.f3e093eb033dacd65693baf218d17efc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "794c7e9658da63d5faeaab94bff69b63",
+    "url": "/kxiao.net/giphy-search/index.html"
+  },
+  {
+    "revision": "d1bcdd0df44f9072eb92",
+    "url": "/kxiao.net/giphy-search/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "d1d100a676cecbba575e",
+    "url": "/kxiao.net/giphy-search/static/css/main.1332489b.chunk.css"
+  },
+  {
+    "revision": "d1bcdd0df44f9072eb92",
+    "url": "/kxiao.net/giphy-search/static/js/2.78bfaed9.chunk.js"
+  },
+  {
+    "revision": "2a4d9b0d8134dea1e9e34f680252bab7",
+    "url": "/kxiao.net/giphy-search/static/js/2.78bfaed9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1d100a676cecbba575e",
+    "url": "/kxiao.net/giphy-search/static/js/main.253e44ea.chunk.js"
+  },
+  {
+    "revision": "3a232169f9f9f31397f5",
+    "url": "/kxiao.net/giphy-search/static/js/runtime-main.b5a65eeb.js"
+  },
+  {
+    "revision": "5c0e699c6c340a413f6b49a02adf2128",
+    "url": "/kxiao.net/giphy-search/static/media/background.5c0e699c.gif"
+  },
+  {
+    "revision": "7043b95a1559cc9ae72bfb85292b2d6e",
+    "url": "/kxiao.net/giphy-search/static/media/giphy.7043b95a.gif"
+  }
+]);
